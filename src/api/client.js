0d7c0b0911e67f01cba910860ca68e4ed080bc95
JC <jcmpes@gmail.com>
2021-09-15 12:26:02 +0200

    feat: Added client.js

---
+++ src/api/client.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const client = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_BASE_URL });
+
+const setAuthorizationHeader = (token) => {
+  client.defaults.headers.common['Authorization'] = `Token ${token}`;
+};
+
+const removeAuthorizationHeader = () => {
+  delete client.defaults.headers.common['Authorization'];
+};
+
+client.interceptors.response.use(
+  (response) => response.data,
+  (error) => {
+    if (!error.response) {
+      return Promise.reject({ message: error.message });
+    }
+    return Promise.reject({
+      message: error.response.statusText,
+      ...error.response,
+      ...error.response.data,
+    });
+  },
+);
+
+export const configureClient = ({ token }) => {
+  if (token) {
+    setAuthorizationHeader(token);
+  }
+};
+
+export const resetClient = () => {
+  removeAuthorizationHeader();
+};
